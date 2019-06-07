101744d78a1e91f8cda2f4de89d99c8fb64c0a75
Tania Ali <taniafali@gmail.com>
2019-06-07 02:15:06 -0700

    Add column names to rendered table

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import Repos from './repos.jsx';
-const RepoList = (props) =>
-  (
+//import Column from './columns.jsx';
+const RepoList = (props) => {
+//let arr = [ "Name","ID","Owner","Git URL", "Created At", "Updated At", "Forks Count" ,"Watchers Count"];
+ return  (
   <div>
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
     <table>
+      {/* {
+        arr.map((prop, k) =>
+        <Column key={k} prop={prop}/>
+      )} */}
+      {/* {props.repos.map((repo, j)=>
+        Object.keys(repo).map((prop, k )=>
+         if (prop ) */}
+          {/* <Column key={k} Key={prop}/>
+        )
+      )} */}
+
+        <th>Name</th>
+        <th>ID</th>
+        <th>Owner</th>
+        <th>Git URL</th>
+        <th>Created At</th>
+        <th>Updated At</th>
+        <th>Forks Count</th>
+        <th>Watchers Count</th>
       <tbody>
         {props.repos.map((repo, i)=>
           // let prop = Object.keys(repo);
@@ -15,6 +36,6 @@ const RepoList = (props) =>
       </tbody>
     </table>
   </div>
-  )
+  )}
 
 export default RepoList;
---
+++ client/src/components/columns.jsx
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+// const Column = ({key, prop}) =>
+//  (
+//   <th key={key}>{prop}</th>
+//  )
+// export default Column;
